05fb414fb006275cd3b2462ac34063e6b0b242a1
MaksimSelin <maksimselin108@gmail.com>
2020-09-26 21:19:45 +0300

    addScreenshot

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -3,8 +3,11 @@ package steps;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import static io.qameta.allure.Allure.getLifecycle;
 import ru.appline.sberbank.managers.DriverManager;
 import ru.appline.sberbank.managers.InitManager;
+import ru.appline.sberbank.utils.MyAllureListener;
 
 public class Hooks {
 
@@ -15,7 +18,10 @@ public class Hooks {
     }
 
     @After
-    public static void close() {
+    public static void close(Scenario scenario) {
+        if (scenario.isFailed()) {
+            getLifecycle().addAttachment("Скриншот","image/png",null, MyAllureListener.addScreenshot());
+        }
         DriverManager.quitDriver();
     }
 }
