359ff67cc527ca30a544b7f1f0119e0b6855d3a7
MaksimSelin <maksimselin108@gmail.com>
2020-09-28 12:01:30 +0300

    edit feature

--- src/main/java/ru/appline/sberbank/pages/MortgageForFinishedHousingPage.java
+++ src/main/java/ru/appline/sberbank/pages/MortgageForFinishedHousingPage.java
@@ -8,10 +8,13 @@ import org.openqa.selenium.support.FindBy;
 import ru.appline.sberbank.entity.Form;
 import ru.appline.sberbank.managers.DriverManager;
 
+
 /**
  * Страница готовое жилье
  */
 public class MortgageForFinishedHousingPage extends BasePage {
+
+    private int k = 0;
 
     @FindBy(xpath = "//h2[text()='\u200BРассчитайте ипотеку']")
     private WebElement formName;
@@ -52,6 +55,7 @@ public class MortgageForFinishedHousingPage extends BasePage {
 
     /**
      * заполняет форму дефолтными значениями
+     *
      * @return
      * @throws InterruptedException
      */
@@ -64,15 +68,15 @@ public class MortgageForFinishedHousingPage extends BasePage {
 
     /**
      * заполняет форму кастомными значениями
-     * @param price - цена
-     * @param anInitialFee - первоначальный взнос
-     * @param creditTerm - срок кредита
+     *
+     * @param price         - цена
+     * @param anInitialFee  - первоначальный взнос
+     * @param creditTerm    - срок кредита
      * @param lifeInsurance - страховка жизни (значения true/false)
-     * @param youngFamily - молодая семья (значения true/false)
+     * @param youngFamily   - молодая семья (значения true/false)
      * @return
      * @throws InterruptedException
      */
-    @Step("Заполнение формы")
     public MortgageForFinishedHousingPage insertForm(String price, String anInitialFee, String creditTerm, String lifeInsurance, String youngFamily) throws InterruptedException {
         Form form = Form.getForm();
         form.setPrice(price);
@@ -84,15 +88,42 @@ public class MortgageForFinishedHousingPage extends BasePage {
         return this;
     }
 
+    public MortgageForFinishedHousingPage insertForm(String fieldName, String value) throws InterruptedException {
+        if (k == 0){
+            setup();
+        }
+        switch (fieldName){
+            case "цена":
+                clearElementAndInsert(priceElement, value);
+                break;
+            case "первоначальный взнос":
+                clearElementAndInsert(anInitialFeeElement, value);
+                break;
+            case "срок кредитования":
+                clearElementAndInsert(creditTermElement, value);
+                break;
+            case "страхование жизни":
+                checkSwitcher(lifeInsuranceElement, value);
+                break;
+            case "молодая семья":
+                checkSwitcher(youngFamilyElement, value);
+                break;
+            default:
+                Assert.fail("Форма не содержет поля '" + fieldName + "'");
+        }
+        return this;
+    }
+
     /**
      * внутренний класс для заполнения формы
+     *
      * @param form - данные для заполнения
      * @throws InterruptedException
      */
     private void insert(Form form) throws InterruptedException {
-        buttonClose.click();
-        scrollToElement(formName);
-        DriverManager.getDriver().switchTo().frame("iFrameResizer0");
+//        buttonClose.click();
+//        scrollToElement(formName);
+//        DriverManager.getDriver().switchTo().frame("iFrameResizer0");
         clearElementAndInsert(priceElement, form.getPrice());
         clearElementAndInsert(anInitialFeeElement, form.getAnInitialFee());
         clearElementAndInsert(creditTermElement, form.getCreditTerm());
@@ -104,6 +135,7 @@ public class MortgageForFinishedHousingPage extends BasePage {
 
     /**
      * внутренний класс для заполнения инпутов формы
+     *
      * @param webElement
      * @param value
      * @throws InterruptedException
@@ -117,6 +149,7 @@ public class MortgageForFinishedHousingPage extends BasePage {
 
     /**
      * внутренний класс для установки состояния переключаателей
+     *
      * @param webElement
      * @param formElement
      */
@@ -124,19 +157,51 @@ public class MortgageForFinishedHousingPage extends BasePage {
         scrollToElement(webElement);
         if (!formElement.equals(webElement.getAttribute("aria-checked")))
             webElement.click();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Проверки
+     *
      * @throws InterruptedException
      */
-    @Step("Проверки")
     public void check() throws InterruptedException {
         Thread.sleep(1000); //элемент меняется динамически, не придумал как отследить.
         Assert.assertEquals("Проверка поля ежемесячный платеж", "16 017 ₽", monthlyPayment.getText());
         Assert.assertEquals("Проверка поля сумма кредита", "2 122 000 ₽", sumCredit.getText());
         Assert.assertEquals("Проверка поля необходимый доход", "20 618 ₽", income.getText());
         Assert.assertEquals("Проверка поля процентная ставка", "11%", interestRate.getText());
+    }
+
+    private void setup(){
+        buttonClose.click();
+        scrollToElement(formName);
+        DriverManager.getDriver().switchTo().frame("iFrameResizer0");
+        k = 1;
+    }
+
+    public void check(String fieldName, String param) {
+        switch (fieldName) {
+            case "ежемесячный платеж":
+                Assert.assertEquals("Проверка поля ежемесячный платеж", "16 017 ₽", monthlyPayment.getText());
+                break;
+            case "сумма кредита":
+                Assert.assertEquals("Проверка поля сумма кредита", "2 122 000 ₽", sumCredit.getText());
+                break;
+            case "необходимый доход":
+                Assert.assertEquals("Проверка поля необходимый доход", "20 618 ₽", income.getText());
+                break;
+            case "процентная ставка":
+                Assert.assertEquals("Проверка поля процентная ставка", "11%", interestRate.getText());
+                break;
+            default:
+                Assert.fail("Поле с наименованием '" + fieldName + "' не найдено");
+        }
+
     }
 
 
--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -1,8 +1,10 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import ru.appline.sberbank.managers.PageManager;
+
 
 public class Steps {
 
@@ -25,13 +27,25 @@ public class Steps {
     }
 
     @When("^Заполнение формы$")
-    public void insertForm() throws InterruptedException {
-        app.getMortgageForFinishedHousingPage().insertForm();
+    public void insertForm(DataTable dataTable) {
+//        app.getMortgageForFinishedHousingPage().insertForm();
+        dataTable.cells().forEach(row -> {
+            try {
+                app.getMortgageForFinishedHousingPage().insertForm(row.get(0), row.get(1) );
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
 
+//    @Then("^Проверка полей$")
+//    public void checkField() throws InterruptedException {
+//        app.getMortgageForFinishedHousingPage().check();
+//    }
+
     @Then("^Проверка полей$")
-    public void checkField() throws InterruptedException {
-        app.getMortgageForFinishedHousingPage().check();
+    public void checkField(DataTable dataTable){
+        dataTable.cells().forEach(row ->app.getMortgageForFinishedHousingPage().check(row.get(0), row.get(1)));
     }
 
 
